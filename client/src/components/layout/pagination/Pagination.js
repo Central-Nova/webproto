8174a219e573af56976e5497b0b40abcf0a9a714
Derrick <dwong4013@gmail.com>
2021-01-15 18:14:02 -0500

    updated pagination

--- client/src/components/layout/pagination/Pagination.js
+++ client/src/components/layout/pagination/Pagination.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PageButton from './PageButton';
 import PageIncrement from './PageIncrement';
 
@@ -6,25 +6,54 @@ import PageIncrement from './PageIncrement';
 
 const Pagination = ({ current, total, onPageChange, onPageIncrement }) => {
 
-  const generatePages = (total) => {
-    let pagesArray = [];
-  
-    for (let i = total; i >= 1; i--) {
-      pagesArray.push(i);
+  const generateLeftPages = (current) => {
+    let leftPages = []
+
+    if (current === 0) {
+      return []
+    } else if (current -3 <= 0) {
+      for (let i = current; i > 0 ; i--) {
+        leftPages.push(i)
+      }
+    } else {
+      for (let i = current; i > current -3 ; i--) {
+        leftPages.push(i)
+      }
+
     }
-  
-    console.log('pagesArray: ', pagesArray);
-    return pagesArray;
+    return leftPages
   }
-  
-  let pagesToRender = generatePages(total);
-  console.log('pagesToRender: ', pagesToRender);
+
+  const generateRightPages = (current, total) => {
+    console.log('current: ', current);
+
+    let rightPages = []
+    if (current + 3 > total) {
+      for (let i = total; i > current; i--) {
+        rightPages.push(i)
+      }
+    } else {
+      for (let i = current + 3; i > current; i--) {
+        rightPages.push(i)
+      }
+    }
+    return rightPages
+  }
+
+  let leftPagesToRender = generateLeftPages(current);
+  let rightPagesToRender = generateRightPages(current, total);
   
   return (
     <div className="container-pagination-grid">
       <div className="buttons">
         <PageIncrement action='next' onChange={onPageIncrement}/>
-        {pagesToRender.length > 0 && pagesToRender.map(number => <PageButton key={number} number={number} current={current} onChange={onPageChange} />)}
+
+        {rightPagesToRender.length > 0 && rightPagesToRender.map(number => 
+        <PageButton key={number} number={number} current={current} onChange={onPageChange} />)}
+
+        {leftPagesToRender.length > 0 && leftPagesToRender.map(number => 
+        <PageButton key={number} number={number} current={current} onChange={onPageChange} />)}
+
         {current !== 0 && <PageIncrement action='back' onChange={onPageIncrement}/>}
       </div>
     </div>
